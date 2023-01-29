d975e39269861111de40a4c3c588e20e53585f2d
Aleksander <alf@umrabco.dk>
2023-01-29 09:10:40 +0100

    added v11 testsite

---
+++ src/Our.Umbraco.Passless.TestSite-v11/Program.cs
@@ -0,0 +1,19 @@
+namespace Our.Umbraco.Passless.TestSite_v11
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+            => CreateHostBuilder(args)
+                .Build()
+                .Run();
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureUmbracoDefaults()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStaticWebAssets();
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
