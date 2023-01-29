016ee1499b4a145a12ee29eb71f60f5b3c584043
Aleksander <alf@umrabco.dk>
2023-01-29 09:24:17 +0100

    check runtime state in migration

--- src/Our.Umbraco.Passless/Credentials/Persistence/MigrationsComponent.cs
+++ src/Our.Umbraco.Passless/Credentials/Persistence/MigrationsComponent.cs
@@ -5,6 +5,7 @@ using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Infrastructure.Migrations;
 using Umbraco.Cms.Infrastructure.Migrations.Upgrade;
 using Our.Umbraco.Passless.Credentials.Persistence.Migrations;
+using Umbraco.Cms.Core;
 
 namespace Our.Umbraco.Passless.Credentials.Persistence;
 
@@ -13,19 +14,26 @@ public class MigrationsComponent : IComponent
     private readonly ICoreScopeProvider scopeProvider;
     private readonly IMigrationPlanExecutor migrationPlanExecutor;
     private readonly IKeyValueService keyValueService;
+    private readonly IRuntimeState runtimeState;
 
     public MigrationsComponent(
         ICoreScopeProvider scopeProvider,
         IMigrationPlanExecutor migrationPlanExecutor,
-        IKeyValueService keyValueService
+        IKeyValueService keyValueService,
+        IRuntimeState runtimeState
         )
     {
         this.scopeProvider = scopeProvider;
         this.migrationPlanExecutor = migrationPlanExecutor;
         this.keyValueService = keyValueService;
+        this.runtimeState = runtimeState;
     }
     public void Initialize()
     {
+        if (runtimeState.Level < RuntimeLevel.Run)
+        {
+            return;
+        }
         var migrationPlan = new MigrationPlan("FidoCredentialsEntity");
         migrationPlan.From(string.Empty)
             .To<AddFidoCredentialEntity>("fidocredetials-db");
