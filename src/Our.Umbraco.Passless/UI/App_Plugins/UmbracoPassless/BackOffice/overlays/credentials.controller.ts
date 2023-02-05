49cfc66169a25bf82e185f486c9ff9b350d865d3
Fjellvang <Aleksander@fjellvang.dk>
2023-02-05 12:59:22 +0100

    Task/type script classes (#7)
    * fixed credential parsing, fixed some TS credential types
    
    * removed wrongly committet 9 project, fixed some TS
    
    * added alot of missing typescript classes, fixed set credentials!
    
    * updated marketplace-json
    
    * update text
    
    ---------
    
    Co-authored-by: Aleksander <alf@umrabco.dk>

--- src/Our.Umbraco.Passless/UI/App_Plugins/UmbracoPassless/BackOffice/overlays/credentials.controller.ts
+++ src/Our.Umbraco.Passless/UI/App_Plugins/UmbracoPassless/BackOffice/overlays/credentials.controller.ts
@@ -1,4 +1,5 @@
 ﻿import { coerceToArrayBuffer, coerceToBase64Url } from "../helpers";
+import { AttestationVerificationSuccess, CredentialMakeResult, UserCredential, UserCredentials } from "../types";
 
 // TODO: define a type for this variable
 declare const Umbraco: any;
@@ -21,8 +22,7 @@ export class CredentialsController {
     private getCredentialsEndpoint: string;
     private deleteCredentialsEndpoint: string;
 
-    // TODO define type
-    private credentials: any;
+    private credentials: UserCredentials | null;
 
     // TODO: define types for the Umbraco services
     constructor(private $scope: ICredentialsControllerScope, private $http: angular.IHttpService, private overlayService: any, private notificationsService: any) {
@@ -37,7 +37,7 @@ export class CredentialsController {
         this.makeCredentialsEndpoint = Umbraco.Sys.ServerVariables.passlessLogin.urls.makeCredentials;
         this.getCredentialsEndpoint = Umbraco.Sys.ServerVariables.passlessLogin.urls.getCredentials;
         this.deleteCredentialsEndpoint = Umbraco.Sys.ServerVariables.passlessLogin.urls.deleteCredentials;
-
+        this.credentials = null;
         this.init();
     }
 
@@ -53,7 +53,7 @@ export class CredentialsController {
     }
 
     // TODO: define a type for the parameter
-    public deleteCredentials(reg: any): void {
+    public deleteCredentials(reg: UserCredential): void {
         this.overlayService.open({
             title: 'Confirm delete credentials',
             content: 'Are you sure you want to delete the credentials?',
@@ -65,8 +65,8 @@ export class CredentialsController {
                     .then((response) => {
                         if (response.status == 200) {
                             this.notificationsService.success('Registration deleted. ' + reg.alias);
-                            const index = this.credentials.userCredentials.indexOf(reg);
-                            this.credentials.userCredentials.splice(index, 1);
+                            const index = this.credentials!.userCredentials.indexOf(reg);
+                            this.credentials!.userCredentials.splice(index, 1);
                         } else {
                             this.notificationsService.error('Error deleting registration.');
                         }
@@ -83,7 +83,7 @@ export class CredentialsController {
 
     public getCredentials(): void {
         this.loading = true;
-        this.$http.get(this.getCredentialsEndpoint)
+        this.$http.get<UserCredentials>(this.getCredentialsEndpoint)
             .then(response => {
                 this.credentials = response.data;
                 this.loading = false;
@@ -97,7 +97,7 @@ export class CredentialsController {
     public submitRegisterPasslessForm(): void {
         console.log(`Submitting ${this.registrationAlias}`)
 
-        this.$http.get(`${this.credentialsOptionsEndpoint}?crossPlatform=${this.crossPlatform}`)
+        this.$http.get<PublicKeyCredentialCreationOptions>(`${this.credentialsOptionsEndpoint}?crossPlatform=${this.crossPlatform}`)
             .then(success => {
                 const makeCredentialsOptions = success.data;
 
@@ -115,19 +115,18 @@ export class CredentialsController {
         }
     }
 
-    // TODO: define parameter type
-    private handleUserCredentials(makeCredentialOptions: any) {
+    private handleUserCredentials(makeCredentialOptions: PublicKeyCredentialCreationOptions) {
         // Turn the challenge back into the accepted format of padded base64
         makeCredentialOptions.challenge = coerceToArrayBuffer(makeCredentialOptions.challenge);
         // Turn ID into a UInt8Array Buffer for some reason
         makeCredentialOptions.user.id = coerceToArrayBuffer(makeCredentialOptions.user.id);
 
-        makeCredentialOptions.excludeCredentials = makeCredentialOptions.excludeCredentials.map((c: any) => {
+        makeCredentialOptions.excludeCredentials = makeCredentialOptions.excludeCredentials?.map((c: any) => {
             c.id = coerceToArrayBuffer(c.id);
             return c;
         });
 
-        if (makeCredentialOptions.authenticatorSelection.authenticatorAttachment === null) makeCredentialOptions.authenticatorSelection.authenticatorAttachment = undefined;
+        if (makeCredentialOptions.authenticatorSelection?.authenticatorAttachment === null) makeCredentialOptions.authenticatorSelection.authenticatorAttachment = undefined;
 
         navigator.credentials.create({
             publicKey: makeCredentialOptions
@@ -139,7 +138,6 @@ export class CredentialsController {
         });
     }
 
-    // TODO: define parameter type
     private prepareNewCredentials(newCredentials: PublicKeyCredential): void {
         // Move data into Arrays incase it is super long
         const attestationObject = new Uint8Array((newCredentials.response as AuthenticatorAttestationResponse).attestationObject);
@@ -147,7 +145,6 @@ export class CredentialsController {
         const rawId = new Uint8Array(newCredentials.rawId);
         const base64UrlRawId = coerceToBase64Url(rawId);
 
-        localStorage.setItem("lastCredentials", newCredentials.id);
 
         const data = {
             id: newCredentials.id,
@@ -170,17 +167,15 @@ export class CredentialsController {
             });
     }
 
-    // TODO: define parameter type
-    private onKeyRegisteredWithServer(credentials: any): void {
-        localStorage.setItem("lastCredentials", credentials.credentialId)
+    private onKeyRegisteredWithServer(credentials: AttestationVerificationSuccess): void {
+        localStorage.setItem("lastCredentials", (credentials.credentialId))
         this.notificationsService.success(`Successfully added new credentials with the alias ${this.registrationAlias}`);
         this.state = 'ready';
         this.getCredentials();
     }
 
-    // TODO: define parameter and return types
-    private registerCredentialWithServer(formData: any): angular.IHttpPromise<any> {
+    private registerCredentialWithServer(formData: any): angular.IHttpPromise<CredentialMakeResult> {
         const body = JSON.stringify(formData);
-        return this.$http.post<any>(`${this.makeCredentialsEndpoint}?alias=${this.registrationAlias}`, body);
+        return this.$http.post<CredentialMakeResult>(`${this.makeCredentialsEndpoint}?alias=${this.registrationAlias}`, body);
     }
 }
